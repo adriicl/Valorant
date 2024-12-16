3fa68ce6baadfffe8c88dd21696f81c76b4e9643
Adrian <adriancabreraleonis172003@gmail.com>
2024-12-16 19:01:31 +0100

    deatalles cambiados de agentes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      backgroundImage: {
+        "hero": "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/7516dbc8273f48844828dfea682d57cd603ee384-1900x1069.jpg?auto=format&fit=fill&q=80&w=1082')"
+      }
+    },
+    plugins: [],
+  }
